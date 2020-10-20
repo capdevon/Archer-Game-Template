80788e1200888a442238864dece24ad9bd864cd5
Wyatt Gillette <jefferydeaver2010@gmail.com>
2020-10-20 12:56:21 +0200

    Update and rename IAnimation.java to AnimDefs.java

--- src/main/java/mygame/AnimDefs.java
+++ src/main/java/mygame/AnimDefs.java
@@ -8,15 +8,21 @@ package mygame;
 import com.capdevon.engine.Animation3;
 import com.jme3.animation.LoopMode;
 
-public interface IAnimation {
+public interface AnimDefs {
     
-    final String MODEL              = "Models/gltf2/Archer/archer.j3o";
+    final String MODEL              = "Models/Archer/archer.j3o";
     final String RIGHT_HAND         = "Armature_mixamorig:RightHandMiddle1";
+    
     final Animation3 Idle           = new Animation3("Idle", LoopMode.Loop, .2f);
     final Animation3 Running        = new Animation3("Running", LoopMode.Loop);
     final Animation3 Running_2      = new Animation3("Running_2", LoopMode.Loop);
+    
     final Animation3 Aim_Idle       = new Animation3("Aim_Idle", LoopMode.DontLoop);
     final Animation3 Aim_Overdraw   = new Animation3("Aim_Overdraw", LoopMode.DontLoop);
     final Animation3 Aim_Recoil     = new Animation3("Aim_Recoil", LoopMode.DontLoop);
     final Animation3 Draw_Arrow     = new Animation3("Draw_Arrow", LoopMode.DontLoop);
+    
+    final Animation3 Water_Idle     = new Animation3("Water_Idle", LoopMode.Loop);
+    final Animation3 Water_Moving   = new Animation3("Water_Moving", LoopMode.Loop);
+    final Animation3 Swimming       = new Animation3("Swimming", LoopMode.Loop);
 }
