af532bc1ccd7b0048b0092773357ecdb9422edb4
Wyatt Gillette <jefferydeaver2010@gmail.com>
2020-11-09 10:15:02 +0100

    Update FVector.java

--- src/main/java/com/capdevon/engine/FVector.java
+++ src/main/java/com/capdevon/engine/FVector.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package xmod.engine;
+package com.capdevon.engine;
 
 import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
