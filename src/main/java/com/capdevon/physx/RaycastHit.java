78dd7f78fea0ee28dafec6f2d78a421d54046f52
Wyatt Gillette <jefferydeaver2010@gmail.com>
2020-11-09 10:31:38 +0100

    Update RaycastHit.java

--- src/main/java/com/capdevon/physx/RaycastHit.java
+++ src/main/java/com/capdevon/physx/RaycastHit.java
@@ -9,19 +9,19 @@ public class RaycastHit {
 
 	public PhysicsCollisionObject rigidbody;
 	public CollisionShape collider;
-    public Spatial userObject;
-    public float distance;
-    public Vector3f normal = new Vector3f();
-    public Vector3f point = new Vector3f();
+	public Spatial userObject;
+	public float distance;
+	public Vector3f normal = new Vector3f();
+	public Vector3f point = new Vector3f();
 
-    public void clear() {
-        rigidbody = null;
-        collider = null;
-        userObject = null;
-        distance = Float.NaN;
-        point.set(Vector3f.NAN);
-        normal.set(Vector3f.NAN);
-    }
+	public void clear() {
+		rigidbody = null;
+		collider = null;
+		userObject = null;
+		distance = Float.NaN;
+		point.set(Vector3f.NAN);
+		normal.set(Vector3f.NAN);
+	}
     
 	@Override
 	public String toString() {
@@ -33,7 +33,7 @@ public class RaycastHit {
 				+ ", point=" + point 
 				+ "]";
 	}
-	
+
 	private String toHexString(Object obj) {
 		if (obj != null)
 			return obj.getClass().getSimpleName() + '@' + Integer.toHexString(obj.hashCode());
