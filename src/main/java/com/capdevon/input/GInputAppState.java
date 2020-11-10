23ff0810d7dafee8a76f3584ffa9f12c292c853d
Wyatt Gillette <jefferydeaver2010@gmail.com>
2020-11-10 11:33:52 +0100

    Update GInputAppState.java

--- src/main/java/com/capdevon/input/GInputAppState.java
+++ src/main/java/com/capdevon/input/GInputAppState.java
@@ -18,16 +18,17 @@ import com.jme3.input.controls.MouseButtonTrigger;
  */
 public class GInputAppState extends AbstractInputAppState {
 
-    @Override
+	@Override
 	public void registerInput() {
 
 		addMapping(KeyMapping.MOVE_FORWARD, 	new KeyTrigger(KeyInput.KEY_W));
 		addMapping(KeyMapping.MOVE_BACKWARD, 	new KeyTrigger(KeyInput.KEY_S));
-		addMapping(KeyMapping.MOVE_LEFT, 		new KeyTrigger(KeyInput.KEY_A));
-		addMapping(KeyMapping.MOVE_RIGHT, 		new KeyTrigger(KeyInput.KEY_D));
-		addMapping(KeyMapping.AIMING, 			new KeyTrigger(KeyInput.KEY_R));
-		addMapping(KeyMapping.RUNNING, 			new KeyTrigger(KeyInput.KEY_SPACE));
-		addMapping(KeyMapping.FIRE, 			new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
+		addMapping(KeyMapping.MOVE_LEFT, 	new KeyTrigger(KeyInput.KEY_A));
+		addMapping(KeyMapping.MOVE_RIGHT, 	new KeyTrigger(KeyInput.KEY_D));
+		addMapping(KeyMapping.AIMING, 		new KeyTrigger(KeyInput.KEY_E));
+		addMapping(KeyMapping.SWITCH_WEAPON, 	new KeyTrigger(KeyInput.KEY_R));
+		addMapping(KeyMapping.RUNNING, 		new KeyTrigger(KeyInput.KEY_SPACE));
+		addMapping(KeyMapping.FIRE, 		new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
 	}
     
     @Override
